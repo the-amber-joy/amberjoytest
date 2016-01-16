833007e938569431079923b507f2d5927ea64735
Amber Joy <amberjoy@Ambers-MacBook-Pro.local>
2016-01-16 14:40:18 -0600

    GitHub content is appearing in the Dom as a gigantic ugly object

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 var gitHubUsername = 'the-amber-joy';
 
-app.controller('MyController', ['$scope', '$http', /*'MyService',*/ 'GitService', function($scope, $http, GitService){
+app.controller('MyController', ['$scope', /*'MyService',*/ 'GitService', function($scope, GitService){
     //$scope.greeting = MyService.greeting;
     //$scope.repoName = MyService.response.data;
     $scope.gitIt = GitService.data;
@@ -12,13 +12,15 @@ app.factory('GitService', ['$http', function($http){
     var gitData = {};
 
     var makeCall = function(){
-        $http.get('/').then(function(response){
-            gitData.results = response.data;
-        });
-
         $http.jsonp('https://api.github.com/users/' + gitHubUsername + '/events?callback=JSON_CALLBACK').then(function(response){
-            console.log(response);
+            gitData.data = response.data.data;
         });
+        //response.results = gitSome.data;
+
+
+        //$http.get('/').then(function(response){
+        //    gitData.results = response.data;
+        //});
     };
 
     return {
--- server/server.js
+++ server/server.js
@@ -8,13 +8,6 @@ app.get('/', function(request, response){
     response.sendFile(__dirname + '/public/views/index.html');
 });
 
-//app.get('/gitSome', function(request, response){
-//    var myGit = $http.jsonp('https://api.github.com/users/' + gitHubUsername + '/events?callback=JSON_CALLBACK').then(function(response){
-//        console.log(response);
-//    });
-//
-//    response.send(myGit);
-//});
 
 var server = app.listen(3000, function(){
     var port = server.address().port;
