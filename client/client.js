afb86654a6c654c181f3220d8853bfa466e84c02
Amber Joy <amber.joy@gmail.com>
2016-04-02 12:09:52 -0500

    clean update

--- client/client.js
+++ client/client.js
@@ -1,20 +1,20 @@
-var app = angular.module('myApp', []); // ['angular.filter'] Still trying to get ths angular filter thing to work...
-var gitHubUsername = 'the-amber-joy';
-
+var app = angular.module('myApp', []); //['angular.filter'] Still trying to get ths angular filter thing to work...
 
 app.controller('MyController', ['$scope', 'GitService', function ($scope, GitService) {
     $scope.gitIt = GitService.data;
     GitService.makeCall();
     }]);
 
-
+// ANGULAR SERVICES can be pulled in ANYWHERE, they return an object... this is like in NODE when we require node module exports - they exist in one place but can be called on from ANYWHERE
 
 app.factory('GitService', ['$http', function($http){
     var gitData = {};
+    var gitHubUsername = 'the-amber-joy';
 
     var makeCall = function(){
         $http.jsonp('https://api.github.com/users/' + gitHubUsername + '/events?callback=JSON_CALLBACK').then(function(response){
             gitData.data = response.data.data;
+            console.log(gitData);
         });
     };
 
@@ -23,3 +23,5 @@ app.factory('GitService', ['$http', function($http){
         data: gitData
     };
 }]);
+
+//factories always expect to output/make public/return their information in the form of OBJECTS
