647543bd7fcf9fff844e6dc98d797ff1ba448450
Amber Joy <amber.joy@gmail.com>
2016-05-19 15:29:12 -0500

    starting to build directive for DOM manipulation

--- client/controllers/BallCtrl.js
+++ client/controllers/BallCtrl.js
@@ -1,5 +1,5 @@
 app.controller('BallCtrl', ['$scope', '$http', function ($scope, $http) {
-
+    $scope.question = '';
     $scope.getAnswer = function(){
         $http.get('/answers').success(function(response){
             $scope.answer = response;
