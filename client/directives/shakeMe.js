4808259c7654fcf90d1de4079236846bd20312fb
Amber Joy <amber.joy@gmail.com>
2016-05-20 00:20:20 -0500

    sample directive working

--- client/directives/shakeMe.js
+++ client/directives/shakeMe.js
@@ -1,26 +1,37 @@
 app.directive('helloWorld', function() {
-  return {
-      restrict: 'AE',
-      replace: 'true',
-      template: '<h3>Hello World!!</h3>'
-  };
-});
-
-
-app.directive('shakeMe', ['$animate', function($animate) {
     return {
-        restrict: 'AEC',
-        link: function(scope, element, attrs, form) {
-            element.on('click', function() {
-
+        restrict: 'AE',
+        replace: true,
+        template: '<p style="background-color:{{color}}">Hello World</p>',
+        link: function(scope, elem, attrs) {
+            elem.bind('click', function() {
+                elem.css('background-color', 'white');
                 scope.$apply(function() {
-
-                    $animate.sibling().addClass(element, 'shake').then(function() {
-
-                        element.sibling().removeClass('shake');
-                    });
+                    scope.color = "white";
                 });
+            });
+            elem.bind('mouseover', function() {
+                elem.css('cursor', 'pointer');
             });
         }
     };
-}]);
+});
+
+
+// app.directive('shakeMe', ['$animate', function($animate) {
+//     return {
+//         restrict: 'AEC',
+//         link: function(scope, element, attrs, form) {
+//             element.on('click', function() {
+//
+//                 scope.$apply(function() {
+//
+//                     $animate.sibling().addClass(element, 'shake').then(function() {
+//
+//                         element.sibling().removeClass('shake');
+//                     });
+//                 });
+//             });
+//         }
+//     };
+// }]);
