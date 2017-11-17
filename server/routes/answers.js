1bd2c5c581aa8244acce5377a798736d6a44d03e
Amber Joy <amber.joy@gmail.com>
2017-11-16 21:15:04 -0600

    updates random answers to umphrey wisdom

--- server/routes/answers.js
+++ server/routes/answers.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
 //var answerList = require('../../data/answers.json');
-var answerList = require('../../data/shamenames.json');
+// var answerList = require('../../data/shamenames.json');
+var answerList = require('../../data/umphreyWisdom.json');
 var randomNumber = require('../../data/randomNumberGenerator.js');
 
 router.get('/', function (req, res) {
 
     //array of answers from JSON
    //var answers = answerList.answers;
-    var answers = answerList.shamenames;
+    var answers = answerList.wisdom;
 
     //pick a random number to decide which answer to send
     var theAnswer = randomNumber(0, answers.length);
 
     //sends one random answer back when called
     // res.send(answers[theAnswer].response);
-    res.send(answers[theAnswer].name);
+    res.send(answers[theAnswer].post);
 
 
 });
